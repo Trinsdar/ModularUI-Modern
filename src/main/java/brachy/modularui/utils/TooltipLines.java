e4139966e28307d134f92cbb4952e67f31c5648b
screret <68943070+screret@users.noreply.github.com>
2026-08-01 16:30:50 +0300

    Make TooltipLines store actual Components instead of FormattedText so REI is happier with our tooltips

--- src/main/java/brachy/modularui/utils/TooltipLines.java
+++ src/main/java/brachy/modularui/utils/TooltipLines.java
@@ -20,7 +20,7 @@ import java.util.List;
 /**
  * A list that lazily parses a list of text-like and drawable types into a vanilla compatible types.
  */
-public class TooltipLines extends AbstractList<Either<FormattedText, TooltipComponent>> {
+public class TooltipLines extends AbstractList<Either<Component, TooltipComponent>> {
 
     private final List<Object> elements;
     private final List<Line> lines = new ArrayList<>(8);
@@ -96,7 +96,7 @@ public class TooltipLines extends AbstractList<Either<FormattedText, TooltipComp
     }
 
     @Override
-    public Either<FormattedText, TooltipComponent> get(int index) {
+    public Either<Component, TooltipComponent> get(int index) {
         buildUntil(index);
         return lines.get(index).text;
     }
@@ -108,7 +108,7 @@ public class TooltipLines extends AbstractList<Either<FormattedText, TooltipComp
     }
 
     @Override
-    public Either<FormattedText, TooltipComponent> remove(int index) {
+    public Either<Component, TooltipComponent> remove(int index) {
         buildUntil(index);
         Line line = lines.remove(index);
 
@@ -126,7 +126,7 @@ public class TooltipLines extends AbstractList<Either<FormattedText, TooltipComp
     }
 
     @Override
-    public void add(int index, Either<FormattedText, TooltipComponent> s) {
+    public void add(int index, Either<Component, TooltipComponent> s) {
         buildUntil(index);
         int elementIndex = index >= this.lines.size() ? this.lastElementIndex : this.lines.get(index).index;
         lines.add(index, new Line(s, elementIndex, 1));
@@ -152,7 +152,7 @@ public class TooltipLines extends AbstractList<Either<FormattedText, TooltipComp
     }
 
     @Override
-    public Either<FormattedText, TooltipComponent> set(int index, Either<FormattedText, TooltipComponent> element) {
+    public Either<Component, TooltipComponent> set(int index, Either<Component, TooltipComponent> element) {
         Line line = lines.get(index);
         element.ifLeft(ft -> {
             if (!(ft instanceof Component)) {
@@ -205,17 +205,17 @@ public class TooltipLines extends AbstractList<Either<FormattedText, TooltipComp
 
     private static class Line {
 
-        private final Either<FormattedText, TooltipComponent> text;
+        private final Either<Component, TooltipComponent> text;
         private final int length;
         private int index;
 
-        private Line(Either<FormattedText, TooltipComponent> text, int index, int length) {
+        private Line(Either<Component, TooltipComponent> text, int index, int length) {
             this.text = text;
             this.index = index;
             this.length = length;
         }
 
-        private Line(FormattedText text, int index, int length) {
+        private Line(Component text, int index, int length) {
             this(Either.left(text), index, length);
         }
 
